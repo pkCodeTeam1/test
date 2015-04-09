27e440603af4e8bd7134eb30bc8ae20f84279d60
pmruk <patryykm@gmail.com>
2015-04-09 09:52:16 +0200

    czysty projekt

---
+++ biblioteka/src/main/java/pl/edu/pk/pkCodeTeam1/biblioteka/App.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.pkCodeTeam1.biblioteka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
